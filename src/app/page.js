387a7c341996538bcafb936647f87c73163eea98
palminister <thanapatj.th@gmail.com>
2023-12-20 13:47:22 +0700

    Add Loader

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,13 @@ import { Lighting } from '@/components/Lighting';
 import { Experience } from '@/components/Experience';
 import { Name } from '@/components/Name';
 import { Postprocessing } from '@/components/Postprocessing';
+import { Html, useProgress } from '@react-three/drei';
 
 export default function Home() {
   return (
     <>
       <Canvas dpr={[1, 2]} shadows>
-        <Suspense fallback={null}>
+        <Suspense fallback={<Loader />}>
           <Experience />
           <Lighting />
           {/* <Postprocessing /> */}
@@ -20,3 +21,15 @@ export default function Home() {
     </>
   );
 }
+
+const Loader = () => {
+  const { active, progress, errors, item, loaded, total } = useProgress();
+  return (
+    <Html
+      center
+      className="flex bg-black text-white font-thin w-screen h-screen justify-center items-center"
+    >
+      {progress.toFixed(2)}% Loaded
+    </Html>
+  );
+};
--- src/components/Dragon.jsx
+++ src/components/Dragon.jsx
@@ -190,3 +190,5 @@ export const Dragon = (props) => {
     </Suspense>
   );
 };
+
+useGLTF.preload('/dragon.glb');
